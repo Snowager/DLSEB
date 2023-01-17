dafc73ae216aecd855d29c4e16c9c927404d0906
tiffanykouakou <tiffanykouakou@gmail.com>
2023-01-17 16:16:17 -0700

    Starting about page

---
+++ about/src/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return <nav className='nav'>
+        <a href='/' className='site-title'>TR!P</a>
+        <ul>
+            <li>
+                <a href='/login'>Login</a>
+            </li>
+            <li>
+                <a href='/register'>Register</a>
+            </li>
+        </ul>
+    </nav>
+}
