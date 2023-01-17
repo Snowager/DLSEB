87d1971c944758f7d43c43a930badceba1a810bd
Snowager <colepr24@gmail.com>
2023-01-16 22:22:22 -0700

    Add Alex's map js applet

---
+++ trip/trip/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import React, {useState, useEffect, useRef} from 'react';
+import MapContainer from './MapContainer.js'; 
+/*global google*/
+
+function App() {
+
+  return (
+    <>
+    <div className='App'>
+      <MapContainer/>
+      </div>
+                        
+      <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initMap&v=weekly"
+      defer
+      ></script>
+      </>
+  );
+}
+
+export default App;
