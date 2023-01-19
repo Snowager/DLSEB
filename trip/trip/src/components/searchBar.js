3f8ce512e553f2ea39e750cecbb39910b8a9394a
Snowager <colepr24@gmail.com>
2023-01-19 08:27:19 -0700

    remove log statements

--- trip/trip/src/components/searchBar.js
+++ trip/trip/src/components/searchBar.js
@@ -7,37 +7,6 @@ const SearchBar = () => {
   const [searchInput, setSearchInput] = useState("");
 
   const [cities, setCities] = useState([])
-  const [lat, setLat] = useState([])
-  const [lng, setLng] = useState([])
-
-  /*const countries = [
-
-    { name: "Belgium", continent: "Europe" },
-    { name: "India", continent: "Asia" },
-    { name: "Bolivia", continent: "South America" },
-    { name: "Ghana", continent: "Africa" },
-    { name: "Japan", continent: "Asia" },
-    { name: "Canada", continent: "North America" },
-    { name: "New Zealand", continent: "Australasia" },
-    { name: "Italy", continent: "Europe" },
-    { name: "South Africa", continent: "Africa" },
-    { name: "China", continent: "Asia" },
-    { name: "Paraguay", continent: "South America" },
-    { name: "Usa", continent: "North America" },
-    { name: "France", continent: "Europe" },
-    { name: "Botswana", continent: "Africa" },
-    { name: "Spain", continent: "Europe" },
-    { name: "Senegal", continent: "Africa" },
-    { name: "Brazil", continent: "South America" },
-    { name: "Denmark", continent: "Europe" },
-    { name: "Mexico", continent: "South America" },
-    { name: "Australia", continent: "Australasia" },
-    { name: "Tanzania", continent: "Africa" },
-    { name: "Bangladesh", continent: "Asia" },
-    { name: "Portugal", continent: "Europe" },
-    { name: "Pakistan", continent: "Asia" },
-
-  ];*/
 
   useEffect(() => {
     axios.get("city_info2.JSON")
@@ -50,8 +19,6 @@ const SearchBar = () => {
     setSearchInput(e.target.value);
     console.log(e.target.value)
   };
-
-  console.log(cities)
 
   const filteredData =
     Object.values(cities).filter((location) => {
