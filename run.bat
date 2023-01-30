828f863401a1ba045c8d0e38f641f9da7f02458a
Snowager <colepr24@gmail.com>
2023-01-29 23:48:19 -0700

    update run.bat

---
+++ run.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+cd trip-home
+cd src
+
+call npm install react-scripts  
+call npm nstall react-google-login --legacy-peer-deps
+call npm nstall @react-google-maps --legacy-peer-deps
+call npm start
