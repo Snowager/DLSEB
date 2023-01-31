acfa3969d4dd8d03f19dc5a3cf4904c2cc3351a3
Snowager <colepr24@gmail.com>
2023-01-31 10:44:05 -0700

    add npm install to run.bat

--- run.bat
+++ run.bat
@@ -6,4 +6,5 @@ cd src
 call npm install react-scripts  
 call npm nstall react-google-login --legacy-peer-deps
 call npm nstall @react-google-maps --legacy-peer-deps
+call npm install gapi-script --legacy-peer-deps
 call npm start
