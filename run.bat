b3acd1c75e8b881338222fa0101e714cdc091682
Snowager <colepr24@gmail.com>
2023-01-31 22:02:56 -0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,7 +4,8 @@ cd trip-home
 cd src
 
 call npm install react-scripts  
-call npm nstall react-google-login --legacy-peer-deps
-call npm nstall @react-google-maps --legacy-peer-deps
+call npm install react-google-login --legacy-peer-deps
+call npm install @react-google-maps --legacy-peer-deps
 call npm install gapi-script --legacy-peer-deps
+call npm install firebase --legacy-peer-deps 
 call npm start
