b5e1382813d444fd3d716af20b26b6b2c8921642
Nate Burdick <89423723+NateBurdick@users.noreply.github.com>
2023-02-07 12:39:55 -0700

    Working searchbar with map on seperate page

--- trip-home/src/scenes/Home/pages/pages/MapPage.js
+++ trip-home/src/scenes/Home/pages/pages/MapPage.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom'
 import MapContainer from '../../../MapCole/components/containers/MapContainer';
+import "../styles/MapPage.css";
 
 
 function MapPage(props) {
-  const [map, setMap] = useState(false)
+  const [map, setMap] = useState(true)
   const location = useLocation()
   const [city, setCity] = useState([]);
   if (location.state) {
@@ -13,7 +14,7 @@ function MapPage(props) {
     return (
       <>
         <h2>{location.state.city}, {location.state.state_name}, looking for a {location.state.type}</h2>
-        <MapContainer lat={location.state.lat} lng={location.state.lng} status={map} zoom={10} />
+        <MapContainer className="Mapp" lat={location.state.lat} lng={location.state.lng} status={map} zoom={10} />
       </>
   );
   }
