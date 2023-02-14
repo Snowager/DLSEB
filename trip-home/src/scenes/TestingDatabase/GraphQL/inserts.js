d16a478c78a5e021a8b3b159aa54336b51a8c0b3
Moosicalex <moosicalex2@gmail.com>
2023-02-14 14:57:58 -0700

    update insert mutations for updated db

--- trip-home/src/scenes/TestingDatabase/GraphQL/inserts.js
+++ trip-home/src/scenes/TestingDatabase/GraphQL/inserts.js
@@ -17,15 +17,24 @@ mutation MyMutation($email: String, $first_name: String, $last_name: String, $pa
   }
 `;
 
+
+export const CREATE_SERVICE = gql`
+mutation MyMutation($address: String, $lat: String, $lng: String, $name: String, $phone_number: numeric, $photo: String, $type: String) {
+  insert_service_one(object: {address: $address, lat: $lat, lng: $lng, name: $name, phone_number: $phone_number, photo: $photo, type: $type}){
+  	address
+  }	
+}
+`;
+
+export const CREATE_IN_TRIP = gql`
+mutation MyMutation($id: Int = 10, $trip_id: String = "") {
+  insert_in_trip_one(object: {id: $id, trip_id: $trip_id}) {
+    trip_id
+    id
+  }
+}
+`;
 /*
-export const CREATE_ = gql`
-
-`;
-
-export const CREATE_ = gql`
-
-`;
-
 export const CREATE_ = gql`
 
 `;
