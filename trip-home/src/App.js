9ad3fe6018cac548e36b2007e00f3ec0b76c77aa
RachelGodina <89423392+RachelGodina@users.noreply.github.com>
2023-02-19 17:53:07 -0700

    Restore login route

--- trip-home/src/App.js
+++ trip-home/src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import SplashPage from './scenes/Home/pages/pages/splash_page';
 import MapPage from './scenes/Home/pages/pages/map_page';
 import About from './scenes/About/pages/pages/about';
-//import Login from './scenes/Sign/pages/pages/login'; //login//
+import Login from './scenes/Sign/pages/pages/login'; //login//
 import Register from './scenes/Sign/pages/pages/register.js';
 //import Reset from './scenes/Sign/pages/pages/reset';
 import Dashboard from './scenes/Sign/pages/pages/dashboard.js';
@@ -15,7 +15,7 @@ function App() {
         <Routes>
         <Route exact path='/' element={< SplashPage />} />
           <Route exact path="/about" element={<About />} />
-          {/*<Route exact path="/login" element={<Login />} />*/}
+          {<Route exact path="/login" element={<Login />} />}
           <Route exact path="/register" element={<Register />} />*/
           {/*<Route exact path="/reset" element={<Reset />} /> */}
           <Route exact path="/dashboard" element={<Dashboard />} />
