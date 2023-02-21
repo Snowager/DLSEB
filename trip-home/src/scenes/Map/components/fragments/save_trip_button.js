4b658645dc9a1aa428452a8b2e22514b10de27ec
Moosicalex <moosicalex2@gmail.com>
2023-02-21 15:59:27 -0700

    change criteria for adding in_trip

--- trip-home/src/scenes/Map/components/fragments/save_trip_button.js
+++ trip-home/src/scenes/Map/components/fragments/save_trip_button.js
@@ -4,17 +4,8 @@ import {CREATE_TRIP, CREATE_IN_TRIP_DB, CREATE_IN_TRIP_GOOGLE} from "../../../Te
 import {GET_TRIP} from "../../../TestingDatabase/GraphQL/queries.js";
 
 const Save_trip_button = (props) => {
-    const dummyItem = {
-        geometry: {
-            location: {
-                lat: function (){return 1},
-                lng: function (){return 1}
-            }
-        }
-    }
     const [trip_id, setTrip_id] = useState("");
     const [trip_list, setTrip_list] = useState([]);
-    const [trip_item, setTrip_item] = useState(dummyItem);
     const [status, setStatus] = useState("");
     const [lat, setLat] = useState("");
     const [lng, setLng] = useState("");
@@ -32,47 +23,58 @@ const Save_trip_button = (props) => {
         return result;
     }
 
+    //adds the most recent trip_id to the trip_list
+    const update_list = () => {
+        setTrip_list([...trip_list, trip_id])
+    }
+
     //creates a function (new_trip) that pushes a new trip entry to the database
-    const [new_trip, trip_loading, trip_error, trip_data] = useMutation(CREATE_TRIP)
+    //Also calls the update_list function once the mutation has successfully been commited to the db with no errors
+    const [new_trip, trip_loading, trip_error, trip_data] = useMutation(CREATE_TRIP, {
+        onCompleted: update_list
+    })
+
+    //creates a function (new_in_trip) that pushes a new in_trip entry to the database
+    const [new_in_trip, in_trip_loading, in_trip_error] = useMutation(CREATE_IN_TRIP_DB)
     
     //pushes a new trip entry every time the trip_id variable is updated
     useEffect(() => {
         console.log("trip_id: " + trip_id);
-        if(trip_id != null){
+        if(trip_id != ""){
+            console.log("pushing new trip to db")
             new_trip({variables:{
                 city:       props.city,
                 user_id:    props.id,
                 trip_id:    trip_id,
                 duration:   props.trip.length
-            }});
+            }})
         }
     }, [trip_id])
-    
-    //creates a function (new_in_trip) that pushes a new in_trip entry to the database
-    const [new_in_trip, in_trip_loading, in_trip_error] = useMutation(CREATE_IN_TRIP_DB)
 
-    const onClick = () => {
-        setTrip_id(makeid(Math.random() * 12 + 6));
+    //pushes new entries for in_trip everytime the trip_list array is updated
+    useEffect(() => {
         if(props.trip != 0){
-            if(trip_id != null){
-                for(const item in props.trip){
-                    new_in_trip({ 
-                        variables: {
-                            service_id: null,
-                            trip_id:    trip_id,
-                            lat:        props.trip[item].geometry.location.lat().toString(),
-                            lng:        props.trip[item].geometry.location.lng().toString(),
-                            loc_name:   props.trip[item].name
-                        }
-                    })
-                    console.log("props lat:" + props.trip[item].geometry.location.lat())
-                    console.log("props lng:" + props.trip[item].geometry.location.lng())
-                }
+            console.log("adding items to trip")
+            for(const item in props.trip){
+                console.log("Adding " + props.trip[item].name + " to the db")
+                console.log("with trip_id:  " + trip_id)
+                new_in_trip({ 
+                    variables: {
+                        service_id: null,
+                        trip_id:    trip_id,
+                        lat:        props.trip[item].geometry.location.lat().toString(),
+                        lng:        props.trip[item].geometry.location.lng().toString(),
+                        loc_name:   props.trip[item].name
+                    }
+                })
+                console.log("props lat:" + props.trip[item].geometry.location.lat() + "props lng:" + props.trip[item].geometry.location.lng())
             }
         }
-        else{
-            console.log("Nothing in the props list");
-        }
+    }, [trip_list])
+    
+    //changes the trip_id every time the button is pressed
+    const onClick = () => {
+        setTrip_id(makeid(Math.random() * 12 + 6));
     }
 
     return(
