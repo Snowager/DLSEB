13d799398c0e0e7aad9c02228094d87e0ed8fc21
Niko-Antuna <70356132+Niko-Antuna@users.noreply.github.com>
2023-02-22 01:18:37 -0700

    Splash Page CSS/Clean Up

--- trip-home/src/scenes/Home/components/fragments/map_search_bar.js
+++ trip-home/src/scenes/Home/components/fragments/map_search_bar.js
@@ -1,5 +1,5 @@
 import { useLoadScript } from "@react-google-maps/api";
-import '../../../Home/pages/styles/map.css';
+import '../../../Home/pages/styles/search_bar.css';
 import '../../../Splash/components/styles/button.css';
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
@@ -37,7 +37,7 @@ const PlacesAutocomplete = ({ setSelected }) => {
         const { lat, lng } = await getLatLng(results[0]);
         const cityName = results[0].address_components[0].long_name
         const stateName = results[0].address_components[2].short_name
-        setSelected({ lat, lng, cityName, stateName});
+        setSelected({ lat, lng, cityName, stateName });
     };
 
     return (
@@ -78,33 +78,32 @@ function MapSearchBar() {
     if (!isLoaded) return <div>Loading...</div>
     return (
         <>
-            <div class="places-container">
+            <div className="places-container">
                 <PlacesAutocomplete setSelected={setSelected} />
             </div>
 
-            <div class="mt-5">
+            <div className="places-container mx-auto">
                 <Link
                     to={`MapPage/restaurant/${setSelected.lat}/${setSelected.lng}`}
-                    className='btn btn--primary'
+                    className='btn btn-light'
                     onClick={() => pushType("restaurant")}
                     state={selected}>
                     Food
                 </Link>
                 <Link
                     to={`MapPage/hotel/${setSelected.lat}/${setSelected.lng}`}
-                    className='btn btn--primary'
+                    className='btn btn-light'
                     onClick={() => pushType("hotel")}
                     state={selected}>
                     Hotel
                 </Link>
                 <Link
                     to={`MapPage/activity/${setSelected.lat}/${setSelected.lng}`}
-                    className='btn btn--primary'
+                    className='btn btn-light'
                     onClick={() => pushType("fun")}
                     state={selected}>
                     Activity
                 </Link>
-
             </div>
         </>
     );
