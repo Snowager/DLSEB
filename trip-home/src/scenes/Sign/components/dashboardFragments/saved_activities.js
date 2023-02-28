48cc92d6d3ea523bd7c5c3ce87cbeb9487254744
Moosicalex <moosicalex2@gmail.com>
2023-02-28 10:21:32 -0700

    change waiting status to loading

--- trip-home/src/scenes/Sign/components/dashboardFragments/saved_activities.js
+++ trip-home/src/scenes/Sign/components/dashboardFragments/saved_activities.js
@@ -40,23 +40,23 @@ const Saved_activities = (props) => {
 
     //once the above query has finished, grab all items in the returned list
     useEffect(() => {
+      console.log("activity status use effect")
       if(activity_status == "complete" && activity_data != undefined){        
         console.log("activity data")
         console.log("here be the user id " + user_id)
         console.log(activity_data)
         setActivities(activity_data.saved_activity)
       }
-      if(activity_status == "waiting"){
+      if(activity_status == "loading"){
         console.log("just wait")
       }
       else{
-        setActivity_status("waiting")
+        setActivity_status("loading")
         console.log("something")
         get_activities({variables: {user_id: 1}, onCompleted: setActivity_status("complete")})
-
       }
     }, [activity_status])
-    
+   
     
     if(activity_loading) return  <div> loading, please hold </div>
     if(activity_error) return    <div> {`Error! ${user_error.message}`}</div>
